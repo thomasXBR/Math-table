57b30763cd2facdc277bb31b0bda975c9022ec20
Thomas <thomas.bulhoes@gmail.com>
2025-04-09 15:02:57 -0300

    Added: -Infinte loop using "while True"

--- math_table.py
+++ math_table.py
@@ -1,14 +1,16 @@
-n = int(input("Insert a whole number: "))
-print('-----------------')
-print(f"TABLE OF {n}")
+while True:
+    n = int(input("Insert a whole number: "))
+    print('-----------------')
+    print(f"TABLE OF {n}")
 
-if n == 0:
-    print("The results of the table of 0 are always euqal to 0")
-    exit()
+    if n == 0:
+        print("The results of the table of 0 are always euqal to 0")
+    else:
+        i = 1
 
-i = 1
+        while i <= 10:
+            for c in range(n, n*10+n, n):
+                print(f'{n} * {i} = {c}')
+                i += 1
+    print('-----------------')
 
-while i <= 10:
-    for c in range(n, n*10+n, n):
-        print(f'{n} * {i} = {c}')
-        i += 1
