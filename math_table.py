d970aa58a93eda04c842f85a615102282e519fcc
Thomas <thomas.bulhoes@gmail.com>
2025-04-09 10:17:12 -0300

    first commit

---
+++ math_table.py
@@ -0,0 +1,14 @@
+n = int(input("Insert a whole number: "))
+print('-----------------')
+print(f"TABLE OF {n}")
+
+if n == 0:
+    print("The results of the table of 0 are always euqal to 0")
+    exit()
+
+i = 1
+
+while i <= 10:
+    for c in range(n, n*10+n, n):
+        print(f'{n} * {i} = {c}')
+        i += 1
